d13e9ec2040aa5ca1009df1e516bc888892783c0
Akash Prasad <78401531+akashprasad7631@users.noreply.github.com>
2023-03-12 18:13:45 +0400

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int> ans;
+        int n= nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            
+            for(int j=i+1;j<n;j++){
+                sum = nums[i]+nums[j];
+                if(sum==target){
+                ans.push_back(i);
+                ans.push_back(j);
+                    break;
+                }
+                
+            } if(ans.size()==2){
+                break;
+            }
+        }
+        return ans;
+    }
+};
